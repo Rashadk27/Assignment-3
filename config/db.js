e835f5ff50a4dd1c7651be8b217024515b152ec0
Rashadk <rashad.khan1@ontariotechu.net>
2025-11-18 11:48:29 -0500

    db.js finished .env file hidden

---
+++ config/db.js
@@ -0,0 +1,18 @@
+// connecting our MONGOSE DATABASE
+
+const mongoose = require('mongoose');
+
+const connectDB = async (mongoURI) => {
+  try {
+    await mongoose.connect(mongoURI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true
+    });
+    console.log('MongoDB connected');
+  } catch (err) {
+    console.error('MongoDB connection error:', err);
+    process.exit(1);
+  }
+};
+
+module.exports = connectDB;
